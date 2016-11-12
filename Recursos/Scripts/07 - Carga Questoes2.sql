a0b188379c8bc31d0edff703841dd428dd3801ab
MeuNoteDELL\PES2016 <marciosao@gmail.com>
2016-11-12 20:18:12 -0300

    ajustes

---
+++ Recursos/Scripts/07 - Carga Questoes2.sql
@@ -0,0 +1,31 @@
+
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(1,2,'Relevância e impactos na Educação, no que se refere à Gestão e/ou a Tecnologia',1,1,2);
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(2,2,'Clareza nos objetivos e do objeto de pesquisa',1,1,2);
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(3,2,'Viabilidade da aplicação: o produto e/ou processo gerado pela proposta de trabalho possui potencial para aplicação e se adequa ao lócus de trabalho do candidato',1,1,2);
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(4,2,'Aderência do objeto de trabalho à Área de Concentração',1,1,2);
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(5,2,'Contextualização e Justificativa: O produto final a ser apresentado se adequa à modalidade de Trabalho de Conclusão adotados pelo GETEC? O trabalho de conclusão do curso poderá ter os seguintes formatos: dissertação, revisão sistemática e aprofundada da literatura, artigo, patente, registros de propriedade intelectual, projetos técnicos, publicações tecnológicas; desenvolvimento de aplicativos, de materiais didáticos e instrucionais e de produtos, processos e técnicas; produção de programas de mídia, editoria, relatórios finais de pesquisa, softwares, estudos de caso, relatório técnico com regras de sigilo, manual de operação técnica, protocolo experimental ou de aplicação em serviços, projeto de aplicação ou adequação tecnológica, protótipos para desenvolvimento ou produção de instrumentos, equipamentos, e kits, projetos de inovação tecnológica. (Regimento GESTEC, Art. 44)',1,1,2);
+
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(6,2,'Iniciação Científica/Tecnológica (bolsista, estagiário, monitor e similares)',2,1,0.5);
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(7,2,'Pós-Graduação lato-sensu (Especialização)',2,1,1);
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(8,2,'Tópico especial cursado em Programas de Pós-Graduação strictu sensu',2,1,0.5);
+
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(9,2,'Docência(Educação Báisca ou Educação Superior)',3,1,1);
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(10,2,'Gestão(institucional, de processos, planos, programas, projetos etc.)',3,1,1);
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(11,2,'Atividade ou tecnológica de natureza instrumental(programador, web-design, modelagem computacional, produtor de mídia, multimídia etc.)',3,1,1);
+
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(12,2,'Trabalhos técnicos (processos, técnicas, produtos tecnológicos, softwares, manuais, patentes, projetos, manuais, assessoria, etc.)',4,1,1.5);
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(13,2,'Participa de Grupo de Pesquisa, tem projetos em andamento ou participa de algum.',4,1,1);
+
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(14,2,'Publicação: artigos, livros e/ou capítulos de livro',5,1,1.25);
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(15,2,'Trabalhos completos e/ou resumos em Anais de Congressos',5,1,0.75);
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(16,2,'Produção acadêmica e aplicada',5,1,0.50);
+
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(17,3,'Domínio sobre a Proposta de Trabalho',6,1,1);
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(18,3,'Aderência da Proposta ao Programa',6,1,1.5);
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(19,3,'Conhecimento atualizado em Gestão e/ou Tecnologia Aplicada à Educação',6,1,2.5);
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(20,3,'Fluência na linguagem técnica relacionada à apresentação da proposta',6,1,2.5);
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(21,3,'Domínio do campo profissional - foco da aplicação da Proposta',6,1,1.5);
+INSERT INTO Questao(`Id`,`IdEtapa`,`Descricao`,`IdCategoriaQuestao`,`IdQuestionario`,`ValorBarema`) VALUES(22,3,'Aderência da proposta à Instituição onde o candidato atua',6,1,1);
+
+
+
