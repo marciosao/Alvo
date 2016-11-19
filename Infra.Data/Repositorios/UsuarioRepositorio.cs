819b2cf711c20629259040d3610d74dc20969adc
MeuNoteDELL\PES2016 <marciosao@gmail.com>
2016-11-18 21:20:06 -0300

    algumas alterações

--- Infra.Data/Repositorios/UsuarioRepositorio.cs
+++ Infra.Data/Repositorios/UsuarioRepositorio.cs
@@ -1,32 +1,35 @@
 ﻿using Dominio.Entidades;
 using Dominio.Interfaces.Repositorios;
+using System;
 using System.Collections;
-using System.Linq;                         
-                                                                                 
-namespace Infra.Data.Repositorios                               
-{                                                                                
-   public class UsuarioRepositorio : RepositorioBase<Usuario>, IUsuarioRepositorio  
+using System.Linq;
+
+namespace Infra.Data.Repositorios
+{
+    public class UsuarioRepositorio : RepositorioBase<Usuario>, IUsuarioRepositorio
     {
         public IEnumerable ObtemUsuariosProfessores()
         {
-            var lProfessores = Db.Usuario.Where(x=>x.Perfil.Id == 2);
+            var lProfessores = Db.Usuario.Where(x => x.Perfil.Id == 2);
 
             return lProfessores;
         }
 
-       public Usuario AutenticarUsuario(Usuario pUsuario)
-       {
-           return this.Entidade().FirstOrDefault(usr => usr.CPF.Equals(pUsuario.CPF) && usr.Senha.Equals(pUsuario.Senha));
-       }
+        public Usuario AutenticarUsuario(Usuario pUsuario)
+        {
+            Usuario teste = new Usuario();
+            var resultado = this.Entidade().FirstOrDefault(usr => usr.CPF.Equals(pUsuario.CPF) && usr.Senha.Equals(pUsuario.Senha));
+            return resultado;
+        }
 
-       public Usuario RecuperarSenha(Usuario usuario)
-       {
-           var usrAlter = Db.Usuario.FirstOrDefault(usr => usr.CPF.Trim().Equals(usuario.CPF.Trim())
-                                                        && usr.Matricula.Trim() == (usuario.Matricula.Trim())
-                                                        && usr.Email.Trim().Equals(usuario.Email.Trim()));
+        public Usuario RecuperarSenha(Usuario usuario)
+        {
+            var usrAlter = Db.Usuario.FirstOrDefault(usr => usr.CPF.Trim().Equals(usuario.CPF.Trim())
+                                                         && usr.Matricula.Trim() == (usuario.Matricula.Trim())
+                                                         && usr.Email.Trim().Equals(usuario.Email.Trim()));
 
-           return usrAlter;
-       }
-    }                                                                            
-}                                                                                
+            return usrAlter;
+        }
+    }
+}
 
